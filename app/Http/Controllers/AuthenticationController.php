b80d069543c0a839ada722f797bf736ca2eabc4f
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-02-17 03:03:37 +0200

    Auth Controller

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
@@ -15,13 +16,51 @@ class AuthenticationController extends Controller
     }
 
     public function register(Request $request){
-        
+        $request->validate([
+           'name' => 'required|max:255|string',
+           'email' => 'required|max:255|string|email|unique:users,email',
+           'password' => 'required|min:8|confirmed',
+        ]);
+
+        $user = $this->userModel::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->accessToken();
+
+        return response([
+            'token' => $token
+        ]);
     }
     public function login(Request $request){
 
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = $this->userModel::where('email',$request->email)->first();
+        if(!$user || !Hash::check($request->password,$user->password)){
+            return response([
+                'message' => 'The provided credentials are incorrect'
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->accessToken();
+
+        return response([
+            'token' => $token
+        ]);
+
     }
     public function logout(Request $request){
+        $request->user()->token()->revoke();
 
+        return response([
+            'message' => 'Logout Successfully!'
+        ]);
     }
 
 
