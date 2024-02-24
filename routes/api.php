b01aac59be66202a464a7deca5093911ece44d1a
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2024-02-24 02:05:01 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register',[AuthenticationController::class,'register'])->name('register');
+Route::post('login',[AuthenticationController::class,'login'])->name('login');
+
+
+
+Route::middleware('auth:api')->group(function(){
+    Route::post('logout',[AuthenticationController::class,'logout'])->name('logout');
+    Route::resource('products',AuthenticationController::class);
+});
+
+
+
+
+
+
+
+
+
+
+
+
